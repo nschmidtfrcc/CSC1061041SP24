c4fbaa1696ae6239ba11a798bd97fbd21f17d9a6
AntonioAndRyu <atorresaguirre@student.cccs.edu>
2024-04-30 12:24:44 -0600

    something

---
+++ Customer.cpp
@@ -0,0 +1,46 @@
+#include "Customer.h"
+#include <iostream>
+#include <vector>
+
+//I: Recieve vector from main and asks the user what they want to display from the vector
+//P: Loops through the vector and picks out the ones that are either interested or not based on what the user selected in the menu
+//O: Outputs the user selected category
+void displayCustomerInfo(vector<Customer> customerPass){
+   int choice = 0;
+   
+   while(choice > 4 || choice < 1){
+      cout << "----MENU----\n" << "  1) Display All\n  2) Display Intersted\n  3) Display Not Interested\n  4) Exit" << endl;
+      cin >> choice;
+      if(choice > 4 || choice < 1){
+         cout << "Invalid option. Please try again";
+      }//input validation
+   }//reprompts the user if they didnt choose something in the menu
+   
+   switch (choice){
+      //prints every object in the customerPass vector
+      case 1:   
+         for(Customer curr : customerPass){
+            curr.printInfo();
+         }//end for loop
+         break;
+         
+      //prints every customer that is interested in the customerPass vector
+      case 2:
+         for(Customer curr : customerPass){
+            if(curr.getBought()){curr.printInfo();}
+         }//end for loop
+         break;
+         
+      //prints every customer that is not interested in the customerPass vector
+      case 3:
+         for(Customer curr : customerPass){
+            if(!curr.getBought()){curr.printInfo();}
+         }//end for loop
+         break;
+         
+      default:
+         break;
+   }//end menu switch
+   
+   return;
+}//end displayCustomerInfo function
