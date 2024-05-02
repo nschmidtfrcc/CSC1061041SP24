5832ab4e13f33f7493d30371b85a24af64ba9371
Xuanhao Li James Deegler <xli37@student.cccs.edu>
2024-05-02 12:14:00 -0600

    stuff

--- vehicleSearch.cpp
+++ vehicleSearch.cpp
@@ -1,49 +1,115 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <fstream>
 using namespace std;
 
 struct Vehicle {
-    string color;
+    string price; //unused (do not delete)
     string make;
+    string color;
+    string vin; //unused (do not delete)
     string model;
-    int year;
-    int timeOnLot; // Time in days
+    string year;
+    string timeOnLot; // Time in days
+    // string blankSpace;
 };
 
 void displayMenu() {
-    cout << "---Vehicle Search Menu---" << endl;
-    cout << "1) All" << endl;
-    cout << "2) Search by color" << endl;
-    cout << "3) Search by make" << endl;
-    cout << "4) Search by model" << endl;
-    cout << "5) Search by year" << endl;
-    cout << "6) Search by time on lot" << endl;
-    cout << "7) Return to main menu" << endl;
+    cout << endl << "Vehicle Search Menu:" << endl;
+    cout << "1-All" << endl;
+    cout << "2-Search by color" << endl;
+    cout << "3-Search by make" << endl;
+    cout << "4-Search by model" << endl;
+    cout << "5-Search by year" << endl;
+    cout << "6-Search by time on lot" << endl;
+    cout << "7-Return to main menu" << endl;
 }
 
-void displayAllVehicles(const vector<Vehicle>& vehicles) {
-    cout << "All Vehicles:" << endl;
-    for (size_t i = 0; i < vehicles.size(); ++i) {
-        cout << "Make: " << vehicles[i].make << ", Model: " << vehicles[i].model << ", Color: " << vehicles[i].color << ", Year: " << vehicles[i].year << ", Time on Lot: " << vehicles[i].timeOnLot << " days" << endl;
+void displayAllVehicles(const vector<string>& vehicles) {
+    cout << endl << "All Vehicles:" << endl;
+    for (size_t i = 1; i < vehicles.size()/7; ++i) {
+        Vehicle car = {vehicles[(i*7)], vehicles[(i*7)+1], vehicles[(i*7)+2], vehicles[(i*7)+3], vehicles[(i*7)+4], vehicles[(i*7)+5], vehicles[(i*7)+6]};
+        cout << "Make: " << car.make << ", Model: " << car.model << ", Color: " << car.color << ", Year: " << car.year << ", Time on Lot: " << car.timeOnLot << " day(s)" << endl;
     }
 }
 
-void searchByColor(const vector<Vehicle>& vehicles, const string& color) {
-    cout << "Vehicles with color " << color << ":" << endl;
-    for (size_t i = 0; i < vehicles.size(); ++i) {
-        if (vehicles[i].color == color) {
-            cout << "Make: " << vehicles[i].make << ", Model: " << vehicles[i].model << ", Year: " << vehicles[i].year << ", Time on Lot: " << vehicles[i].timeOnLot << " days" << endl;
+void searchByColor(const vector<string>& vehicles, const string& color) {
+    cout << endl << "Vehicles with color of " << color << ":" << endl;
+    for (size_t i = 1; i < vehicles.size()/7; ++i) {
+        Vehicle car = {vehicles[(i*7)], vehicles[(i*7)+1], vehicles[(i*7)+2], vehicles[(i*7)+3], vehicles[(i*7)+4], vehicles[(i*7)+5], vehicles[(i*7)+6]};
+        if (car.color == color) {
+            cout << "Make: " << car.make << ", Model: " << car.model << ", Color: " << car.color << ", Year: " << car.year << ", Time on Lot: " << car.timeOnLot << " day(s)" << endl;
+        }
+    }
+}
+
+void searchByMake(const vector<string>& vehicles, const string& make) {
+    cout << endl << "Vehicles with make of " << make << ":" << endl;
+    for (size_t i = 1; i < vehicles.size()/7; ++i) {
+        Vehicle car = {vehicles[(i*7)], vehicles[(i*7)+1], vehicles[(i*7)+2], vehicles[(i*7)+3], vehicles[(i*7)+4], vehicles[(i*7)+5], vehicles[(i*7)+6]};
+        if (car.make == make) {
+            cout << "Make: " << car.make << ", Model: " << car.model << ", Color: " << car.color << ", Year: " << car.year << ", Time on Lot: " << car.timeOnLot << " day(s)" << endl;
+        }
+    }
+}
+
+void searchByModel(const vector<string>& vehicles, const string& model) {
+    cout << endl << "Vehicles with model type of " << model << ":" << endl;
+    for (size_t i = 1; i < vehicles.size()/7; ++i) {
+        Vehicle car = {vehicles[(i*7)], vehicles[(i*7)+1], vehicles[(i*7)+2], vehicles[(i*7)+3], vehicles[(i*7)+4], vehicles[(i*7)+5], vehicles[(i*7)+6]};
+        if (car.model == model) {
+            cout << "Make: " << car.make << ", Model: " << car.model << ", Color: " << car.color << ", Year: " << car.year << ", Time on Lot: " << car.timeOnLot << " day(s)" << endl;
+        }
+    }
+}
+
+void searchByYear(const vector<string>& vehicles, const string& year) {
+    cout << endl << "Vehicles with year of " << year << ":" << endl;
+    for (size_t i = 1; i < vehicles.size()/7; ++i) {
+        Vehicle car = {vehicles[(i*7)], vehicles[(i*7)+1], vehicles[(i*7)+2], vehicles[(i*7)+3], vehicles[(i*7)+4], vehicles[(i*7)+5], vehicles[(i*7)+6]};
+        if (car.year == year) {
+            cout << "Make: " << car.make << ", Model: " << car.model << ", Color: " << car.color << ", Year: " << car.year << ", Time on Lot: " << car.timeOnLot << " day(s)" << endl;
+        }
+    }
+}
+
+void searchByTime(const vector<string>& vehicles, const string& time) {
+    cout << endl << "Vehicles with time on lot of " << time << ":" << endl;
+    for (size_t i = 1; i < vehicles.size()/7; ++i) {
+        Vehicle car = {vehicles[(i*7)], vehicles[(i*7)+1], vehicles[(i*7)+2], vehicles[(i*7)+3], vehicles[(i*7)+4], vehicles[(i*7)+5], vehicles[(i*7)+6]};
+        if (car.timeOnLot == time) {
+            cout << "Make: " << car.make << ", Model: " << car.model << ", Color: " << car.color << ", Year: " << car.year << ", Time on Lot: " << car.timeOnLot << " day(s)" << endl;
         }
     }
 }
 
 // Implement other search functions similarly
 
-int main() {
-    vector<Vehicle> vehicleList;
+int vehicleSearch(vector<string> vehicleListPre) {
+    vector<string> vehicleList = vehicleListPre;
     // Initialize your list of vehicles here by pushing elements into the vector
+
+    // Open and read file (this will push vehicle values into the vector, use this if passed in vector is not formatted properly or if no vector is passed into main)
+    vehicleList.clear();
+    ifstream fin;
+    string file_name = "vehicleList.txt";
+    fin.open(file_name.c_str());
+
+    string line;
+
+    while(getline(fin, line))
+    {
+        vehicleList.push_back(line);
+    }
+    fin.close();
+    // End of read file
+
+    // Insert "-"x7 for additional formatting
+    for(int i = 0; i < 7; i++)
+        vehicleList.insert(vehicleList.begin(), "-");
+    // End
+
 
     int choice;
     string searchTerm;
@@ -63,11 +129,31 @@ int main() {
                 searchByColor(vehicleList, searchTerm);
                 break;
             // Implement other cases similarly
+            case 3:
+                cout << "Enter make: ";
+                cin >> searchTerm;
+                searchByMake(vehicleList, searchTerm);
+                break;
+            case 4:
+                cout << "Enter model: ";
+                cin >> searchTerm;
+                searchByModel(vehicleList, searchTerm);
+                break;
+            case 5:
+                cout << "Enter year: ";
+                cin >> searchTerm;
+                searchByYear(vehicleList, searchTerm);
+                break;
+            case 6:
+                cout << "Enter time on lot: ";
+                cin >> searchTerm;
+                searchByTime(vehicleList, searchTerm);
+                break;
             case 7:
-                cout << "Returning to main menu." << endl;
+                cout << endl << "Returning to main menu." << endl;
                 break;
             default:
-                cout << "Invalid choice. Please try again." << endl;
+                cout << endl << "Invalid choice. Please try again." << endl;
                 break;
         }
     } while (choice != 7);
