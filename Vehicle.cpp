51222eafa33844891f964044f5df5950420e065c
Tyler & Indira <thomchanh2@student.cccs.edu>
2024-05-02 11:35:41 -0600

    add LotTime to displayInfo()

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -4,7 +4,7 @@
 //this had a lot of duplicate getters and setters that were already in the vehicle.h so i cleaned it up -CL
 
 void Vehicle::displayInfo() {
-   std::cout << "Vehicle: " << make << ", " << model <<  ", " << year << ", " << color << ", $" << price << std::endl;
+   std::cout << "Vehicle: " << make << ", " << model <<  ", " << year << ", " << color << ", " << lotTime << ", $" << price << std::endl;
 }
 
 void Vehicle::PrintInfo() {
