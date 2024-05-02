cb48400cad8789dae25d4d6c5c242df27156b6f5
Tyler & Indira <thomchanh2@student.cccs.edu>
2024-05-02 11:49:36 -0600

    clean end of main

--- main.cpp
+++ main.cpp
@@ -135,7 +135,7 @@ int main()   {
 
         std::cout << userIn;
     }
-<<<<<<< HEAD
+
    /*
    for(int i=0; i<vehicle.size();++i){
       vehicle[i].displayInfo();
@@ -144,10 +144,4 @@ int main()   {
    
    return 0;
 }//end main()
-=======
-   
-   
-   
-   return 0;
-}//end main()
->>>>>>> main
+
