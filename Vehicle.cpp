3b3002bb0993c1e410b54eec659e98e997e1bb54
Sultan-Marley <mhall207@student.cccs.edu>
2024-05-02 12:12:52 -0600

    Altered file reading, added sold vector, partial enum conversion - MH

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -3,6 +3,15 @@
 #include "Vehicle.h"
 //this had a lot of duplicate getters and setters that were already in the vehicle.h so i cleaned it up -CL
 
+Color Vehicle::stringToEnumConverter(const std::string& str) { // Copied from CL's changes - MH
+    auto it = stringToEnum.find(str);
+    if (it != stringToEnum.end()) {
+        return it->second;
+    }
+    // Handle unknown string, maybe throw an exception or return a default value
+    return Red; // Default to Red for unknown strings
+}
+
 void Vehicle::displayInfo() {
    std::cout << "Vehicle: " << make << ", " << model <<  ", " << year << ", " << color << ", $" << price << std::endl;
 }
--- Vehicle.h
+++ Vehicle.h
@@ -1,9 +1,13 @@
 #pragma once
 #ifndef VEHICLE_H
 #define VEHICLE_H
-
+#include <unordered_map>
 #include <string>
 
+enum Color {
+		Red, Blue, White, Silver, Black
+	};
+   
 class Vehicle {
 private:
 	int price;
@@ -11,10 +15,6 @@ private:
 	std::string make;
 	int year;
 	std::string VIN;
-	enum Color {
-		Red, Blue, White, Silver, Black
-	};
-	Color color;
 	int lotTime;
    bool sold;
 
@@ -25,7 +25,7 @@ public:
 	std::string getMake() { return make; }
 	int getYear() { return year; }
 	std::string getVIN() { return VIN; }
-	Color getColor() { return color; }
+	Color getColor() { return Color; }
 	int getTime() { return lotTime; }
 
 	//Setters (inline)
@@ -34,7 +34,7 @@ public:
 	void setMake(std::string make) { this->make = make; }
 	void setYear(int year) { this->year = year; }
 	void setVIN(std::string VIN) { this->VIN = VIN; }
-	void setColor(Color color) { this->color = color; }
+	void setColor(Color) { this->Color = Color; }
 	void setTime(int time) { lotTime = time; }
 
 	//Constructors
@@ -43,11 +43,22 @@ public:
 	////Main constructor; should be the one we use
 	Vehicle(int price, std::string model, std::string make, int year, std::string VIN, Color color, int lotTime);
    
+   //Necessary for file reading -CL
+	static Color stringToEnumConverter(const std::string& str);
+   
    //print info of car
 	void displayInfo();
    
 	//PrintInfo fx; prints ALL info in an organized manner
 	void PrintInfo();
+};
+
+const std::unordered_map<std::string, Color> stringToEnum = {
+    {"Red", Red},
+    {"Blue", Blue},
+    {"White", White},
+    {"Silver", Silver},
+    {"Black", Black}
 };
 
 #endif
